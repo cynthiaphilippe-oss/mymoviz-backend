975f10478ce414c61d2af0aa5a5622298c8bda32
Cynthia <cynthia.philippe@gmail.com>
2026-07-08 12:25:56 +0200

    Initial commit backend

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+const OWM_API_KEY = "bf8556d627ea4333ffa3711f34c6f2e6";
+
+/* GET home page */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET movies from TMDB */
+router.get('/movies', async (req, res) => {
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}`);
+  const data = await response.json();
+
+  res.json({ movies: data.results });
+});
+
+module.exports = router;
