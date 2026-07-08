0cad9e50b913a00134bfb48820bdc519468532ae
Cynthia <cynthia.philippe@gmail.com>
2026-07-08 14:35:04 +0200

    maj urls.js

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'mymoviz-backend-etnyc.vercel.app';
+const BACKEND_URL = 'https://mymoviz-backend-eight-theta.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'mymoviz-frontend-etnyc.vercel.app';
+const FRONTEND_URL = 'https://mymoviz-frontend-etnyc.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
